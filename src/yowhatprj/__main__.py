e64faa0b64609d9c4ef8623c56a660f6cddd403e
devin <devinwrx@gmail.com>
2024-08-02 10:00:00 -0400

    playing with linting and testing pipelines

--- src/yowhatprj/__main__.py
+++ src/yowhatprj/__main__.py
@@ -3,5 +3,7 @@ from yowhatprj.yoyoyo.yo import say_yo as s # Absolute
 import httpx
 
 def main() -> None:
+    with httpx.Client as client:
+        print(client)
     print(what.say_what())
     print(s())
